e0fc66a0c609105e31a9a4bb40a0a36b4137bb85
scx1332 <sieciech.czajka@golem.network>
2024-05-28 20:53:14 +0200

    work in progress

--- gnoui/gnoui.js
+++ gnoui/gnoui.js
@@ -79,10 +79,137 @@ async function getTransactionsIds(contract, pending) {
         resp = await contract.getTransactionIds(0, transCount, false, true);
         for (let i = 0; i < resp.length; i++) {
             document.getElementById('executed-transactions').innerHTML += `<li>Executed: ${resp[i]} </li>`;
+            try {
+                await getTransactionDetails(contract, resp[i]);
+            } catch (e) {
+                console.error(e);
+            }
         }
     }
     console.log(resp);
 }
+
+async function downloadAbi(network, contractAddress) {
+    let abiUrl;
+    if (network === "mainnet") {
+        abiUrl = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}`;
+    } else if (network === "holesky") {
+        abiUrl = `https://api-holesky.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}`;
+    } else {
+        console.error(`Invalid network ${network}`);
+        return;
+    }
+
+    let localStorageKey = `abi_${network}_${contractAddress}`;
+    let localStorageItem = localStorage.getItem(localStorageKey);
+    if (localStorageItem) {
+        return localStorageItem;
+    }
+
+    for (let i = 0; i < 3; i++) {
+        let response = await fetch(abiUrl);
+        if (response.status !== 200) {
+            console.error("Error fetching ABI");
+        } else {
+            let abi = await response.json();
+            if (abi.message && abi.message.includes("Invalid API key")) {
+                console.warn("Rate limit exceeded trying again");
+            }
+            let abiJson = JSON.parse(abi["result"]);
+            let abiString = JSON.stringify(abiJson, null, 2);
+            console.log("Success downloading ABI: " + abiString);
+            localStorage.setItem(localStorageKey, abiString);
+            return abiString;
+        }
+    }
+}
+
+async function getTransactionDetails(contract, transactionId) {
+    let resp = await contract.transactions(transactionId);
+
+    let targetAddr = resp[0];
+    let value = resp[1];
+    let data = resp[2];
+    //data from hex
+    let executed = resp[3];
+
+    console.log("Transaction details: " + targetAddr + " " + value + " " + data + " " + executed);
+    if (!data.startsWith('0x')) {
+        throw "Data must start with 0x";
+    }
+
+    let bytes = looseArrayify(data);
+    // check if Uint8Array
+    if (!bytes instanceof Uint8Array) {
+        throw "Data must be Uint8Array";
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = "transaction-details";
+    if (bytes.length > 0) {
+        let abi = await downloadAbi("holesky", targetAddr);
+        let iface = new Interface(abi);
+        let func = iface.getFunction(hexlify(bytes.subarray(0, 4)));
+        console.log("Called: " + func);
+        let decoded = iface.decodeFunctionData(func, hexlify(bytes));
+        {
+            let div = document.createElement('div');
+            div.innerHTML = `Target address: <a href="https://holesky.etherscan.io/address/${targetAddr}">${targetAddr}</a>`;
+            newDiv.appendChild(div);
+        }
+        {
+            let div = document.createElement('div');
+            div.innerText = `Gas (ETH) transferred: ${value}`;
+            newDiv.appendChild(div);
+        }
+        let fullFuncSig = func.name + "(";
+        for (let i = 0; i < func.inputs.length; i++) {
+            fullFuncSig += `${func.inputs[i].type},`;
+        }
+        fullFuncSig = fullFuncSig.slice(0, -1) + ")";
+        {
+            let div = document.createElement('div');
+            div.innerHTML = `Method signature: <span class="function-signature">${fullFuncSig}</span>`;
+            newDiv.appendChild(div);
+        }
+        // Print decoded values and their types
+        for (let i = 0; i < decoded.length; i++) {
+            {
+                let div = document.createElement('div');
+                div.className = "decoded-param";
+                div.innerText = `Param no ${i + 1} (${func.inputs[i].type}): ${decoded[i]}`;
+                newDiv.appendChild(div);
+            }
+        }
+        {
+            let div = document.createElement('div');
+            div.innerText = `Call data: ${data}`;
+            newDiv.appendChild(div);
+        }
+
+        document.getElementById('transaction-details').appendChild(newDiv);
+    } else {
+        {
+            let div = document.createElement('div');
+            div.innerText = `Target address: ${targetAddr}`;
+            newDiv.appendChild(div);
+        }
+        {
+            let div = document.createElement('div');
+            div.innerText = `Value: ${value}`;
+            newDiv.appendChild(div);
+        }
+        {
+            let div = document.createElement('div');
+            div.innerText = `Call data: ${data}`;
+            newDiv.appendChild(div);
+        }
+    }
+
+
+
+}
+
+
 
 
 async function get_chain_id() {
@@ -155,6 +282,55 @@ function addOwner() {
     });
 }
 
+async function sendErc20Token() {
+    let tokenAddress = document.getElementById('token-address').value;
+    let destinationAddress = document.getElementById('token-dest-address').value;
+
+    let erc20 = new ethers.Contract(tokenAddress, erc20abi, new ethers.BrowserProvider(provider))
+
+    let tokenName = await erc20.name();
+    let tokenSymbol = await erc20.symbol();
+    let decimalPlaces = await erc20.decimals();
+
+    let amount = document.getElementById('token-transfer-value').value;
+
+    let bigAmount = parseUnits(amount, decimalPlaces);
+    let bigAmountHex = '0x' + bigAmount.toString(16);
+    bigAmount = BigInt(bigAmountHex);
+
+
+    let confirmStr = "Are you sure you want to send \n";
+    confirmStr += `token (${tokenName} (${tokenSymbol}), decimals: ${decimalPlaces}, address: ${tokenAddress}) \n`;
+    confirmStr += `${formatUnits(bigAmount, decimalPlaces)} ${tokenSymbol} (dec: ${bigAmount.toString()}, hex: ${bigAmountHex}) \n`;
+    confirmStr += `to ${destinationAddress}`;
+
+
+    if (confirm(confirmStr)) {
+        let multisigAddress = "0x7D7222f0A7d95E43d9D960F5EF6F2E5d2A72aC59";
+        let iface = new Interface(gnosisAbi);
+        let erc20iface = new Interface(erc20abi);
+
+        let calldata = erc20iface.encodeFunctionData("transfer", [destinationAddress, bigAmount]);
+        let gasLimit = 1000000;
+        let gasLimitHex = gasLimit.toString(16);
+        let call = iface.encodeFunctionData("submitTransaction", [tokenAddress, 0, calldata]);
+        window.ethereum.request({
+            "method": "eth_sendTransaction",
+            "params": [
+                {
+                    "to": multisigAddress,
+                    "from": connected,
+                    "gas": gasLimitHex,
+                    "value": "0x0",
+                    "data": call,
+                }
+            ]
+        });
+    }
+
+
+
+}
 function sendGasTransfer() {
     let address = document.getElementById('gas-destination-address').value;
     let amount = document.getElementById('gas-transfer-value').value;
@@ -164,7 +340,7 @@ function sendGasTransfer() {
     bigAmount = BigInt(bigAmountHex);
 
     let confirmStr = "Are you sure you want to send \n";
-    confirmStr += `${formatEther(bigAmount)} ETH (${bigAmount.toString()} wei, encoded ${bigAmountHex}) \n`;
+    confirmStr += `${formatEther(bigAmount)} ETH (dec: ${bigAmount.toString()} wei, hex: ${bigAmountHex}) \n`;
     confirmStr += `to ${address}`;
     if (confirm(confirmStr)) {
         let multisigAddress = "0x7D7222f0A7d95E43d9D960F5EF6F2E5d2A72aC59";
