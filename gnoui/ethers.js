d9bcac18c3b94897ec81059d975e1272e0e98e31
scx1332 <sieciech.czajka@golem.network>
2024-06-30 12:29:57 +0200

    removing stuff from ethers

--- gnoui/ethers.js
+++ gnoui/ethers.js
@@ -11978,44 +11978,9 @@ function injectCommonNetworks() {
             });
         }
     }
-    registerEth("mainnet", 1, { ensNetwork: 1, altNames: ["homestead"] });
-    registerEth("ropsten", 3, { ensNetwork: 3 });
-    registerEth("rinkeby", 4, { ensNetwork: 4 });
-    registerEth("goerli", 5, { ensNetwork: 5 });
-    registerEth("kovan", 42, { ensNetwork: 42 });
-    registerEth("sepolia", 11155111, { ensNetwork: 11155111 });
-    registerEth("holesky", 17000, { ensNetwork: 17000 });
-    registerEth("classic", 61, {});
-    registerEth("classicKotti", 6, {});
-    registerEth("arbitrum", 42161, {
-        ensNetwork: 1,
-    });
-    registerEth("arbitrum-goerli", 421613, {});
-    registerEth("arbitrum-sepolia", 421614, {});
-    registerEth("base", 8453, { ensNetwork: 1 });
-    registerEth("base-goerli", 84531, {});
-    registerEth("base-sepolia", 84532, {});
-    registerEth("bnb", 56, { ensNetwork: 1 });
-    registerEth("bnbt", 97, {});
-    registerEth("linea", 59144, { ensNetwork: 1 });
-    registerEth("linea-goerli", 59140, {});
-    registerEth("linea-sepolia", 59141, {});
-    registerEth("matic", 137, {
-        ensNetwork: 1,
-        plugins: []
-    });
-    registerEth("matic-amoy", 80002, {});
-    registerEth("matic-mumbai", 80001, {
-        altNames: ["maticMumbai", "maticmum"],
-        plugins: []
-    });
-    registerEth("optimism", 10, {
-        ensNetwork: 1,
-        plugins: []
-    });
-    registerEth("optimism-goerli", 420, {});
-    registerEth("optimism-sepolia", 11155420, {});
-    registerEth("xdai", 100, { ensNetwork: 1 });
+    registerEth("mainnet", 1, {});
+    registerEth("sepolia", 11155111, {});
+    registerEth("holesky", 17000, {});
 }
 
 function copy$2(obj) {
